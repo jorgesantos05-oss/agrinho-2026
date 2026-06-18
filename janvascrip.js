35a50e4ea8b86a76ea4932777e870c14cc4fd352
jorgesantos05-oss <jorge.santos05@escola.pr.gov.br>
2026-06-18 09:01:23 -0300

    Add files via upload

---
+++ janvascrip.js
@@ -0,0 +1,9 @@
+function mostrarMensagem() {
+
+    const texto = document.getElementById("texto");
+
+    texto.innerHTML =
+    "Com drones, sensores, satélites e inteligência artificial, o produtor rural pode tomar decisões mais rápidas, aumentar a produtividade e preservar o meio ambiente.";
+
+    texto.style.color = "#00ff66";
+}
